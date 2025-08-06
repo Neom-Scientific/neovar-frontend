3f963bc3368bf987ef9cdb8c91fd36e3e8987b0d
Prateek khatri <pkprateekkhatri@gmail.com>
2025-08-06 17:19:45 +0530

    chnaged the next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,38 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url)); // Define __dirname for ES modules
+// const __dirname = path.dirname(fileURLToPath(import.meta.url)); // Define __dirname for ES modules
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone', // Export static files
-  outputFileTracingRoot: __dirname, // Ensure this points to the project root
-    outputFileTracingIncludes: {
-      '/': ['./src/app/globals.css'], // Ensure globals.css is included
-    },
-  webpack(config) {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      '@components': path.resolve(__dirname, 'components'),
-    };
-    return config;
-  },
-
-  images: {
-    unoptimized: true, // Required for static export
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-};
-
-export default nextConfig;
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+// const nextConfig = {
+//   output: 'standalone', // Export static files
+//   outputFileTracingRoot: __dirname, // Ensure this points to the project root
+//     outputFileTracingIncludes: {
+//       '/': ['./src/app/globals.css'], // Ensure globals.css is included
+//     },
+//   webpack(config) {
+//     config.resolve.alias = {
+//       ...(config.resolve.alias || {}),
+//       '@components': path.resolve(__dirname, 'components'),
+//     };
+//     return config;
+//   },
+
+//   images: {
+//     unoptimized: true, // Required for static export
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//         port: '',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// };
 
 // export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
